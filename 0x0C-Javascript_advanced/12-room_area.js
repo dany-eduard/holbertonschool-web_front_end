b345b45ee78d0d71a97d961b734c75bdf5881930
dany-eduard <danyeduard17@gmail.com>
2021-09-21 22:04:03 -0500

    add: task #12. Binding

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,18 @@
+/**
+ * @description - Task 12
+ * @var {object} roomDimensions
+ * @function getArea - function that returns the surface area of a the object using the width and length
+ * @returns {Number} surface area of a the object using the width and length
+ * @var {Number} boundGetArea
+ */
+let roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea() {
+    return this.width * this.length;
+  },
+};
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+
+console.log(roomDimensions.getArea());
